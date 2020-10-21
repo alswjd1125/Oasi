77bc8fe18760cbb4f968e43aa4cc755c1e264a6a
alswjd1125 <alslal1125@naver.com>
2020-10-21 18:43:34 +0900

    Oasi
    Oasi

--- assets/js/main.js
+++ assets/js/main.js
@@ -131,18 +131,52 @@
     time: 1000
   });
 
+  // Skills section
+  $('.skills-content').waypoint(function() {
+    $('.progress .progress-bar').each(function() {
+      $(this).css("width", $(this).attr("aria-valuenow") + '%');
+    });
+  }, {
+    offset: '80%'
+  });
 
+  // Porfolio isotope and filter
+  $(window).on('load', function() {
+    var portfolioIsotope = $('.portfolio-container').isotope({
+      itemSelector: '.portfolio-item',
+      layoutMode: 'fitRows'
+    });
+    $('#portfolio-flters li').on('click', function() {
+      $("#portfolio-flters li").removeClass('filter-active');
+      $(this).addClass('filter-active');
+      portfolioIsotope.isotope({
+        filter: $(this).data('filter')
+      });
+    });
+    // Initiate venobox (lightbox feature used in portofilo)
+    $(document).ready(function() {
+      $('.venobox').venobox();
+    });
+  });
 
-  // review
+  // Portfolio details carousel
+  $(".portfolio-details-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    items: 1
+  });
+
+  // Testimonials carousel (uses the Owl Carousel library)
   $(".testimonials-carousel").owlCarousel({
     autoplay: true,
     dots: true,
     loop: true,
     responsive: {
-      500: {
+      0: {
         items: 1
       },
-      700: {
+      768: {
         items: 2
       },
       900: {
@@ -150,3 +184,5 @@
       }
     }
   });
+
+})(jQuery);
